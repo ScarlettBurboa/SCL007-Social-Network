2c22ab188f61315a08b7de3a97e51d324849890a
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-02-13 22:26:04 -0300

    función buscar funcionando en consola, mas diseño css a botones editar/borrar y otros arrreglos css

--- src/js/data.js
+++ src/js/data.js
@@ -1,3 +1,4 @@
+
 /* función para guardar datos (post) en la base de datos Firebase */
 export const savePost = (userName, post, photo, datePost) => {
     const userId = firebase.auth().currentUser.uid;
@@ -18,23 +19,18 @@ export const savePost = (userName, post, photo, datePost) => {
     });
     };
      /*_______________________________________________________________________- */
+export let objectComplete = [];
+firebase.database().ref('post').orderByValue().on("value", function(snapshot) {
+    Object.entries(snapshot.val()).forEach(function(data) {
+    objectComplete.push(data[1]);
+    });      
+});    
 export const readPost = (postChange) =>{
     const postReference = firebase.database().ref('post/');
     postReference.on('child_added', (post) => {
         postChange(post);
     });   
 };
-//export const savePostUser = (userName, post, photo, datePost) => {
-//    const userId = firebase.auth().currentUser.uid;
-//    const keyPost = firebase.database().ref('postUser/').child('postUser').push()//.key;    
-//       firebase.database().ref(`postUser/${userId}/${keyPost}/`).set({
-//           user : userName, 
-//           pospublic : post, 
-//           userphoto : photo,
-//          createdDate: datePost,
-//       });
-//       
-//       };
 export const readPostUser = (postChange) =>{
     const userId = firebase.auth().currentUser.uid;
     const postReferenceread = firebase.database().ref(`postUser/${userId}/`);
@@ -42,4 +38,17 @@ export const readPostUser = (postChange) =>{
            postChange(postUser);
        });
    };
+   /*_________________________________________________________________________*/
+   //Función para buscar elementos por sus keys.
+export function findObjectByKey(array, key, value) {
+    const ret = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][key].includes(value)) {
+            ret.push(array[i]);
+        }
+    }
+    return ret;
+}
+
+
 
